28a298258e225caf7ae6bbc32f0ff753509fd406
altrano <altrano@altramail.net>
2010-03-07 04:58:02 +0100

    add default stylesheet.

--- modules/alGuestbook/actions/components.class.php
+++ modules/alGuestbook/actions/components.class.php
@@ -9,6 +9,9 @@ class alGuestbookComponents extends myFrontModuleComponents
 
   public function executeList()
   {
+    /* add default css */
+    $this->getResponse()->addStylesheet('/alGuestbookPlugin/css/al_guestbook.css');
+    
     $query = $this->getListQuery();
     
     $this->alGuestbookPager = $this->getPager($query);
